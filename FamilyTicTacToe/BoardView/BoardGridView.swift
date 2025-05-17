d18a732c6406309e4a0f996435461cc39aabd130
Lamar Williams <lamar@moofus.com>
2025-05-17 01:10:22 -0700

    progress

---
+++ FamilyTicTacToe/BoardView/BoardGridView.swift
@@ -0,0 +1,18 @@
+//
+//  BoardGridView.swift
+//  FamilyTicTacToe
+//
+//  Created by Lamar Williams III on 5/17/25.
+//
+
+import SwiftUI
+
+struct BoardGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BoardGridView()
+}
